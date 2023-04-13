62c9e7243b1aae8b1367bf0453f24cd92a1a9e2d
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2023-04-13 10:46:47 +0800

    Create App.java

---
+++ Inheritance/Hybrid/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        
+    System.out.println(Sample.preference);
+    System.out.println(Sample.taste);
+    System.out.println(Sample.drinks);
+    
+    }
+}
